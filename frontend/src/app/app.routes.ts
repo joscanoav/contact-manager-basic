423fb24531fb4dad7a72815d5ef4c3fe7b4eb856
joscanoav <joscanoav@gmail.com>
2025-07-03 22:40:50 +0200

    set up routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 // Importamos el tipo Route para definir rutas en Angular
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
 // Importamos los componentes que usaremos en cada ruta
 import { ContactListComponent } from './contacts/components/contact-list/contact-list.component';
 import { ContactAddComponent } from './contacts/components/contact-add/contact-add.component';
 
 // Definimos el array de rutas de la aplicación
-export const routes: Route[] = [
+export const routes: Routes = [
   // Ruta raíz (vacía): redirige a 'contacts'
   // pathMatch: 'full' indica que la URL debe coincidir exactamente con ''
   { path: '', redirectTo: 'contacts', pathMatch: 'full' },
